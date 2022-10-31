adfd1061f42011fad994b64158264466d87ec17e
Dakota hall <Hallda020292@gmail.com>
2022-10-31 12:00:19 -0400

    first commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+
+            var r = 20;
+            var radius = r;
+            var pi = Math.PI;  
+         
+            var areaOfCircle = CalculateArea(radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {areaOfCircle}");
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return Math.PI * (radius * radius);
         }
     }
+
 }
